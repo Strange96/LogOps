396230a025762039631c290ee4a1fe2751391725
Hr. Strange <msh@cardlay.com>
2019-12-18 22:38:03 +0100

    WIP

--- runLogOps.sh
+++ runLogOps.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
+# CREATE SHARED NETWORK
+docker network create l_o_net
+
 # START BY INITIALIZING DBs
 cd Log-Store && docker-compose up -d esdb && cd ..
 cd log-alarm-service && docker-compose up -d mongo && cd ..
 
-# START LOG-STORE, ALARM-SERVICE, JOLIE-EXECUTOR, AGENT-SERVICE AND SUBSCRIPTION-SERVICE
+# START LOG-STORE, ALARM-SERVICE, JOLIE-CLOUD, AGENT-SERVICE AND SUBSCRIPTION-SERVICE
 cd Log-Store && docker-compose up -d logstore && cd ..
 cd log-alarm-service && docker-compose up -d alarmservice && cd ..
-cd Log-Jolie-Cloud && docker-compose up -d && cd ..
-cd Log-Agent && bash -c 'python3 Server.py &' > /dev/null && cd Files && bash -c 'python3 Agent.py & > /dev/null' > /dev/null && cd ../..
-cd Log-Auth && docker-compose up -d && cd ..
+#cd Log-Jolie-Cloud && docker-compose up -d && cd ..
+#cd Log-Agent && bash -c 'python3 Server.py &' > /dev/null && cd Files && bash -c 'python3 Agent.py company42 license42 & > /dev/null' > /dev/null && cd ../..
+#cd Log-Auth && docker-compose up -d && cd ..
+
+# CONNECT SERVICES TO SHARED NETWORK
+docker network connect l_o_net logstore
+docker network connect l_o_net alarmservice
+#docker network connect l_o_net parsermanager
+#docker network connect l_o_net authservice
 
 # EXPORT IPS OF SERVICES
-LOGSTORE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' logstore)
-ALARMSERVICE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' alarmservice)
-JOLIECLOUD_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' parsermanager)
-AUTHSERVICE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' authservice)
+export GLOBAL_URL=localhost
+export LOGSTORE_URL=logstore:8080
+export ALARMSERVICE_URL=alarmservice:8085
+#export JOLIECLOUD_URL=parsermanager:8001
+#export AUTHSERVICE_URL=authservice:7900
+
+#LOGSTORE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' logstore)
+#ALARMSERVICE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' alarmservice)
+#JOLIECLOUD_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' parsermanager)
+#AUTHSERVICE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' authservice)
 
 # START WEBSITE
-cd Log-Website && docker-compose up -d && cd ..
+cd Log-Website && docker-compose up -d && docker network connect l_o_net php && cd ..
 
-echo $'\nPress any to stop LogOps - but why would you? ;-)'
+echo $'\nPress any key to stop LogOps - but why would you? ;-)'
 read -n 1 -s
 
 cd Log-Website && docker-compose down && cd ..
-cd Log-Auth && docker-compose down && cd ..
-kill -9 $(ps aux | grep Server.py | awk {'print $2'} | head -1)
-kill -9 $(ps aux | grep Agent.py | awk {'print $2'} | head -1)
-cd Log-Jolie-Cloud && docker-compose down && cd ..
+#cd Log-Auth && docker-compose down && cd ..
+#kill -9 $(ps aux | grep Server.py | awk {'print $2'} | head -1)
+#kill -9 $(ps aux | grep Agent.py | awk {'print $2'} | head -1)
+#cd Log-Jolie-Cloud && docker-compose down && cd ..
 cd log-alarm-service && docker-compose down && cd ..
 cd Log-Store && docker-compose down && cd ..
+
+#unset AUTHSERVICE_URL
+#unset JOLIECLOUD_URL
+unset ALARMSERVICE_URL
+unset LOGSTORE_URL
+unset GLOBAL_URL
+
+docker network rm l_o_net
