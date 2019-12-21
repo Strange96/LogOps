ff63c276fe38ab6b6cd222aae2323e922c15c2a4
Hr. Strange <msh@cardlay.com>
2019-12-21 02:31:30 +0100

    back to small working example

--- old_runLogOps.sh
+++ old_runLogOps.sh
@@ -17,30 +17,21 @@ cd Log-Store && docker-compose up -d logstore && cd ..
 cd log-alarm-service && docker-compose up -d alarmservice && cd ..
 cd Log-Jolie-Cloud && docker-compose up -d && cd ..
 #cd Log-Agent && bash -c 'python3 Server.py &' > /dev/null && cd Files && bash -c 'python3 Agent.py company42 license42 & > /dev/null' > /dev/null && cd ../..
-cd Log-Auth && docker-compose up -d && cd ..
+#cd Log-Auth && docker-compose up -d && cd ..
 
 # CONNECT SERVICES TO SHARED NETWORK
 docker network connect l_o_net logstore
 docker network connect l_o_net alarmservice
 docker network connect l_o_net parsermanager
 docker network connect l_o_net builder
-docker network connect l_o_net subscription
+#docker network connect l_o_net subscription
 
 # EXPORT IPS OF SERVICES
-#export NGINX_GLOBAL_URL=http://localhost:80
-export NGINX_LOGSTORE_URL=http://localhost:8079
-export NGINX_ALARMSERVICE_URL=http://localhost:8085
-export NGINX_JOLIECLOUD_URL=http://localhost:8001
-export NGINX_AUTHSERVICE_URL=http://localhost:7900
-export NGINX_GETPARSERHOST_URL=http://localhost:8006
-
-#export GLOBAL_URL=http://localhost
-#export LOGSTORE_URL=http://localhost:8079
-#export ALARMSERVICE_URL=http://localhost:8085
-#export JOLIECLOUD_URL=http://localhost:8001
+export GLOBAL_URL=http://localhost
+export LOGSTORE_URL=http://localhost:8079
+export ALARMSERVICE_URL=http://localhost:8085
+export JOLIECLOUD_URL=http://localhost:8001
 #export AUTHSERVICE_URL=http://localhost:7900
-#export php_ADDR=php
-#export MYSQL_ADDR=mysql
 
 #LOGSTORE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' logstore)
 #ALARMSERVICE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' alarmservice)
@@ -48,7 +39,7 @@ export NGINX_GETPARSERHOST_URL=http://localhost:8006
 #AUTHSERVICE_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' authservice)
 
 # START WEBSITE
-cd Log-Website && docker-compose up -d && docker network connect l_o_net php && docker network connect l_o_net nginx && cd ..
+cd Log-Website && docker-compose up -d && docker network connect l_o_net php && cd ..
 
 echo $'\nPress any key to stop LogOps - but why would you? ;-)'
 read -n 1 -s
@@ -62,18 +53,9 @@ cd log-alarm-service && docker-compose down && cd ..
 cd Log-Store && docker-compose down && cd ..
 
 #unset AUTHSERVICE_URL
-#unset JOLIECLOUD_URL
-#unset ALARMSERVICE_URL
-#unset LOGSTORE_URL
-#unset MYSQL_ADDR
-#unset php_ADDR
-#unset GLOBAL_URL
-
-#unset NGINX_GLOBAL_URL
-unset NGINX_LOGSTORE_URL
-unset NGINX_ALARMSERVICE_URL
-unset NGINX_JOLIECLOUD_URL
-unset NGINX_AUTHSERVICE_URL
-unset NGINX_GETPARSERHOST_URL
+unset JOLIECLOUD_URL
+unset ALARMSERVICE_URL
+unset LOGSTORE_URL
+unset GLOBAL_URL
 
 docker network rm l_o_net
